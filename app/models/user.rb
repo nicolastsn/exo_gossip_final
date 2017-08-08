752081e9088107d952c9a4e61031b9d2c396ee31
Nicolas <nicolas.tsan@gmail.com>
2017-08-08 16:54:44 +0200

    sign up ok

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+validates :anonymous_username, presence:true, length: {maximum: 60}
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+
+	root 'home#index'
+  
+  devise_for :users
+  get 'home/index'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
